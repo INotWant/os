cfd1979aeb018cae0d8430d59cd4c8f88a054787
INotWant <tinggao@bupt.edu.cn>
2020-10-26 21:37:53 +0800

    load_disk & gdt

---
+++ boot/gdt.asm
@@ -0,0 +1,37 @@
+;;;;;;;;;;;;;;;;;;;
+; desc: GDT
+; data: 2020.10.26
+;;;;;;;;;;;;;;;;;;;
+
+
+gdt_start:      ; GDT 以 8字节 null 开始
+    dd 0x0      ; 4 byte
+    dd 0x0      ; 4 byte
+
+gdt_code:       ; 代码段，8字节
+    dw 0xffff   ; 段长度，bits 0-15
+    dw 0x0      ; 段基址，bits 0-15
+    db 0x0      ; 段基址，bits 16-23
+    db 10011010b; 标志（8bits）-> 
+    db 11001111b; 标志（4bits）+ 段长度（bits 16-19）
+    db 0x0      ; 段基址，bits 24-31
+
+gdt_data:       ; 数据段，8字节
+    dw 0xffff
+    dw 0x0
+    db 0x0
+    db 10010010b
+    db 11001111b
+    db 0x0
+
+gdt_end:
+
+
+; GDT 描述符
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1  ; gdt 大小，总是小 1
+    dd gdt_start                ; gdt 地址
+
+; 一些常量
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
---
+++ boot/load_disk.asm
@@ -0,0 +1,44 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; desc: 实现“实模式”下从磁盘加载数据至内存
+; data: 2020.10.26
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;; 功能：实模式下从磁盘加载数据至内存
+;;; 参数：dh 指定被加载至内存的扇区数目，dl 指定从哪个（哪种）存储设备加载
+disk_load:
+    pusha
+    push dx
+
+    mov ah, 0x02    ; 指定 'read'
+    mov al, dh      ; 指定扇区数目
+    mov cl, 0x02    ; 指定从哪个扇区开始（0x01 是 boot 扇区）
+    mov ch, 0x00    ; 指定柱面
+    mov dh, 0x00    ; 指定磁头号
+
+    int 0x13        ; BIOS 中断 --> https://stanislavs.org/helppc/int_13-2.html
+    jc disk_error
+
+    pop dx
+    cmp al, dh      ; al 被设置为加载的扇区数
+    jne sectors_error
+    popa
+    ret
+
+disk_error:
+    mov bx, DISK_ERROR
+    call print
+    call print_nl
+    mov dh, ah      ; ah 存储了错误码 -> dx 作为 print_hex 参数
+    call print_hex
+    jmp disk_loop
+
+sectors_error:
+    mov bx, SECTORS_ERROR
+    call print
+
+disk_loop:
+    jmp $
+
+DISK_ERROR: db "Disk read error", 0
+SECTORS_ERROR: db "Incorrect number of sectors read", 0
