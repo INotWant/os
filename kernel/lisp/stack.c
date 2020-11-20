e903b221affce9dd1d6b3871ca54807196c8ce01
INotWant <tinggao@bupt.edu.cn>
2020-11-20 11:24:47 +0800

    optimize stack

--- kernel/lisp/stack.c
+++ kernel/lisp/stack.c
@@ -1,18 +1,7 @@
 #include "stack.h"
 
-/* 栈底指针 */
-static void *stack_bottom_point;
-
-uint8_t stack_init() {
-    element_t element;
-    element.type = POINT_PAIR_T;
-    element.val.point = 0;
-    void *pp = cons(&element, &element);
-    if (pp == 0)
-        return 0;
-    stack_top_point = pp;
-    stack_bottom_point = pp;
-    return 1;
+void stack_init() {
+    stack_top_point = 0;
 }
 
 uint8_t push(element_t *element_point) {
@@ -28,7 +17,7 @@ uint8_t push(element_t *element_point) {
 
 element_t pop() {
     element_t element;
-    if (stack_top_point == stack_bottom_point){
+    if (stack_top_point == 0){
         element.type = POINT_PAIR_T;
         element.val.point = 0;
         return element;
